cc8cdc48c47d095b0c3ade0f926b1555ddf12df9
ai20260226-hue <ai.2026.02.26@gmail.com>
2026-03-01 13:54:16 +0900

    Add identity loading functionality from JSON

--- core/role.py
+++ core/role.py
@@ -1,1 +1,10 @@
+import json
+from core.github_sync import get_file
 
+
+IDENTITY_PATH = "nation/zaibatsu/identity.json"
+
+
+def load_identity():
+    content, _ = get_file(IDENTITY_PATH)
+    return json.loads(content) if content else {}
